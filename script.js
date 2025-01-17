1c694bd073e9f737161805f0f427d06212d3ab6c
Shaik Asif <bhanurshaikasif@gmail.com>
2025-01-17 13:04:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-function getFormvalue() {
-    //Write your code here
+function getFormvalue(event) {
+    // Prevent the default form submission behavior
+    event.preventDefault();
 
+    // Get the form element
+    const form = document.getElementById('form1');
+
+    // Retrieve the values of 'fname' and 'lname'
+    const firstName = form.elements['fname'].value;
+    const lastName = form.elements['lname'].value;
+
+    // Display the first and last name in an alert
+    alert(`${firstName} ${lastName}`);
 }
